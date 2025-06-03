35cb74071834df2c7edb9583728a88bcbdd42831
NuclearRedeye <joel.summerfield@thirdfort.com>
2025-06-03 20:42:23 +0100

    feat: add initial form renderer

---
+++ mobile/src/components/TaskFormTaskStep.tsx
@@ -0,0 +1,61 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from '@react-navigation/elements';
+
+import { FormTaskStepParams } from '../../libs/genproto/thirdfort/consumer/tasksteps/type/v1/form_pb';
+
+import { spacing, typography, scheme } from '../styles/values';
+
+type TaskCompletionTaskStepProps = {
+  params: FormTaskStepParams;
+  onTaskStepComplete: () => void;
+};
+
+export function TaskFormTaskStep({ params, onTaskStepComplete } : TaskCompletionTaskStepProps) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.descriptionContainer}>
+          <Text style={[styles.containerText, styles.title]}>
+            {params.displayName}
+          </Text>
+          <Text style={[styles.containerText, styles.description]}>
+            {params.description}
+          </Text>
+        </View>
+        <Button onPress={onTaskStepComplete}>{params.submitButtonText}</Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerText: {
+    color: scheme.body1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: scheme.background1,
+  },
+  content: {
+    flex: 1,
+    marginTop: 40,
+    gap: spacing.lg,
+    padding: spacing.lg,
+    justifyContent: 'space-between',
+  },
+  icon: {
+    marginTop: 10,
+    width: 32,
+    height: 32,
+  },
+  descriptionContainer: {
+    paddingVertical: spacing.lg,
+    gap: spacing.md,
+  },
+  title: {
+    ...typography.title,
+  },
+  description: {
+    ...typography.paragraph,
+  },
+});
--- mobile/src/mapping.ts
+++ mobile/src/mapping.ts
@@ -3,12 +3,15 @@ import { TaskDescriptionTaskStep } from './components/TaskDescriptionTaskStep';
 import { TaskDescriptionTaskStepParamsSchema } from '../libs/genproto/thirdfort/consumer/tasksteps/type/v1/task-description_pb';
 import { TaskInformationTaskStep } from './components/TaskInformationTaskStep';
 import { TaskInformationTaskStepParamsSchema } from '../libs/genproto/thirdfort/consumer/tasksteps/type/v1/task-information_pb';
+import { TaskFormTaskStep } from './components/TaskFormTaskStep';
+import { FormTaskStepDataSchema } from '../libs/genproto/thirdfort/consumer/tasksteps/type/v1/form_pb';
 import { TaskCompletionTaskStep } from './components/TaskCompletionTaskStep';
 import { TaskCompletionTaskStepParamsSchema } from '../libs/genproto/thirdfort/consumer/tasksteps/type/v1/task-completion_pb';
 
 type TaskStepParamName =
   | 'TaskDescription'
   | 'TaskInformation'
+  | 'TaskForm'
   | 'TaskCompletion'
 
 type TaskStepMap = {
@@ -27,6 +30,10 @@ export const taskStepMap: TaskStepMap = {
   'TaskInformation': {
     component: TaskInformationTaskStep,
     paramsSchema: TaskInformationTaskStepParamsSchema,
+  },
+  'TaskForm': {
+    component: TaskFormTaskStep,
+    paramsSchema: FormTaskStepDataSchema,
   },
   'TaskCompletion': {
     component: TaskCompletionTaskStep,
